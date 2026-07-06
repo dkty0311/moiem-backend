8f69fd2c8ce81d19737de993bb3f7c043f7dc2d1
macintaesh <dkty0311@naver.com>
2026-07-07 00:30:21 +0900

    커밋 테스트

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hellooooooooo World!';
+    return 'what the hell World!';
   }
 }
